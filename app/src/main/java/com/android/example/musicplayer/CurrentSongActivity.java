5645e00a7305a10e64e03be6b43de06ec3b805ab
Kevin <notmyrealemail0000@gmail.com>
2021-12-08 13:25:05 -0500

    Updated based on required code review

--- app/src/main/java/com/android/example/musicplayer/CurrentSongActivity.java
+++ app/src/main/java/com/android/example/musicplayer/CurrentSongActivity.java
@@ -3,27 +3,30 @@ package com.android.example.musicplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.example.musicplayer.databinding.ActivityCurrentSongBinding;
+
 public class CurrentSongActivity extends AppCompatActivity {
+
+    ActivityCurrentSongBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_song);
+        binding = binding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         //Place Holder Song
         Song currentSong = new Song("Why Why", "Shannon Williams", "5:49", R.drawable.why_why);
 
         //Set views displayed to the appropriate attributes
-        TextView currentSongName = findViewById(R.id.textview_current_song_name);
-        currentSongName.setText(currentSong.getName());
-
-        TextView currentSongArtist = findViewById(R.id.textview_current_artist);
-        currentSongArtist.setText(currentSong.getArtist());
-
-        ImageView currentSongImage = findViewById(R.id.image_current_song);
-        currentSongImage.setImageResource(currentSong.getImage());
+        binding.textviewCurrentSongName.setText(currentSong.getName());
+        binding.textviewCurrentArtist.setText(currentSong.getArtist());
+        binding.imageCurrentSong.setImageResource(currentSong.getImage());
     }
 
 }
